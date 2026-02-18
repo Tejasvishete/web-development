c53f2a2cea54511b4d676c5d40d55df4850fee38
tejasvishete <tejasvishete499@gmail.com>
2026-02-18 14:39:05 +0530

    test code

---
+++ script.js
@@ -0,0 +1,51 @@
+// let a;
+// let b;
+// a=20;
+// b=30;
+// console.log("addition is:",a+b);
+// console.log("subtraction is:",a-b);
+// console.log("multiplication is:",a*b);
+// console.log("division is:",a/b);
+// console.log("power  is:",a**b);
+
+
+
+// let age=10;
+
+// if(age>=18){
+//                                         console.log("eligible for voting")
+// }
+// else{
+//                                         console.log("not eligible")
+// }
+
+
+
+
+
+let marks=100;
+
+if(marks>=500){
+                                        console.log("First class")
+}
+
+
+else if(marks>=400){
+                                        console.log("second class")
+}
+else if(marks>=250){
+                                        console.log("pass")
+}
+else{
+                                        console.log("fail")
+}
+
+
+
+let n=10;
+if(n/2==0){
+                                        console.log("even")
+}
+else{
+                                        console.log("odd")
+}

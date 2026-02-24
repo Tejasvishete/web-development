9a18d5f413faad3aed2fd4cab8a58f439993e8a0
tejasvishete <tejasvishete499@gmail.com>
2026-02-24 15:25:52 +0530

    test code

---
+++ star.js
@@ -0,0 +1,83 @@
+// for( let i=1; i<=5; i++){
+                                        
+//                                         console.log("*");
+// }
+
+
+// for (let i=1; i<=5; i++){
+//                                         let row="";
+//                                         for(let j=1;j<=i;j++){
+//                                            row+="*";                                   
+//                                         }
+//                                         console.log(row);
+// }
+
+
+// for (let i=5; i>=1; i--){
+//                                         let row="";
+//                                         for(let j=1;j<=i;j++){
+                                                                                
+//                                            row +="*";                                    
+//                                         }
+//                                         console.log(row);
+// }
+
+
+// for (let i=5; i>=1; i--){
+//                                         let row="";
+//                                         for(let j=1;j<=i;j++){
+//                                                                                console.log("*")
+//                                            row +="*";                                    
+//                                         }
+//                                         console.log(row);
+// }
+
+
+// for (let i=1; i<=5; i++){
+//                                         let row="";
+//                                         for(let j=1;j<=i;j++){
+//                                                                                console.log("*")
+//                                            row +="*";                                    
+//                                         }
+//                                         console.log(row);
+// }
+
+// let n=5;
+// let star="*";
+// let space=" ";
+
+
+// for (let i=1; i<=n;i++){
+//                                         let row="";
+//                                         for( let s=1;s<=n-i;s++){
+//                                                                                 row+="";
+                                                                               
+
+//                                         }
+//                                         for(let j=1; j<=i; j++){
+//                                                                                 row +="* ";
+//                                         }
+//                                         console.log(row)
+// }
+
+
+let n=5;
+let star="*";
+let space=" ";
+
+
+for (let i=n; i>=1;i--){
+                                        let row="";
+                                        for( let s=1;s<= n-i;s++){
+                                                                                row+=" ";
+                                                                               
+
+                                        }
+                                        for(let j=1; j<=i; j++){
+                                                                                row +="* ";
+                                        }
+                                        console.log(row)
+}
+
+
+
---
+++ test.js
@@ -0,0 +1,15 @@
+a2=500;
+for(let i=0; i<7; i++){
+                                        let beep=(i*i*i)
+                                        a2=a2-beep;
+                                        console.log(a2)
+}
+
+
+let base=500
+console.log(base);
+for(let i=1; i<=10;i++){
+                                        let result=i**3;
+                                        base=base-result;
+                                        console.log(base);
+}
